67f1c6f359385a27adea1dfd6dc3f6d30cc220ee
rajesh <peketirajesh007@gmail.com>
2023-08-05 18:34:55 +0530

    navigation completd

---
+++ src/components/Navs.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+
+const LINKS = [
+    {
+        text:"home",
+        to:"/",
+    },
+    {
+        text:"starred",
+        to: "/starred",
+    }
+]
+
+
+const Navs = () =>{
+    return (
+        <div>
+            <ul>
+                {LINKS.map((item)=>(
+                    <li key={item.to}>
+                        <Link to={item.to}>{item.text}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Navs
